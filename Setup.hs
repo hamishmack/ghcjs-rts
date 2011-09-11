75b5245311b2c04657340de1a0dec58eba2c8b8a
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2011-09-12 06:48:19 +1200

    Use copyHook as well as installHook

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,5 @@
-import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, instHook)
-import Distribution.Simple.Setup (CopyDest(..), installVerbosity, fromFlag)
+import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, instHook, copyHook)
+import Distribution.Simple.Setup (CopyDest(..), installVerbosity, copyVerbosity, fromFlag)
 import Distribution.Simple.Utils (createDirectoryIfMissingVerbose, installOrdinaryFile)
 import Distribution.Simple.LocalBuildInfo (buildDir, absoluteInstallDirs, InstallDirs(..))
 import System.FilePath ((</>))
@@ -8,14 +8,28 @@ main = defaultMainWithHooks simpleUserHooks {
     instHook = (\pkg_descr lbi hooks flags -> do
         -- Run the normal install
         instHook simpleUserHooks pkg_descr lbi hooks flags
-        
+
         -- Also copy the JavaScript RTS files
         let destination = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
             verbosity   = fromFlag (installVerbosity flags)
             copy n      = installOrdinaryFile verbosity n (destination </> n)
-        
+
         createDirectoryIfMissingVerbose verbosity True destination
         copy "rts-common.js"
         copy "rts-plain.js"
         copy "rts-trampoline.js")
-    }
+
+  , copyHook = (\pkg_descr lbi hooks flags -> do
+        -- Run the normal copy
+        copyHook simpleUserHooks pkg_descr lbi hooks flags
+
+        -- Also copy the JavaScript RTS files
+        let destination = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
+            verbosity   = fromFlag (copyVerbosity flags)
+            copy n      = installOrdinaryFile verbosity n (destination </> n)
+
+        createDirectoryIfMissingVerbose verbosity True destination
+        copy "rts-common.js"
+        copy "rts-plain.js"
+        copy "rts-trampoline.js")
+  }
